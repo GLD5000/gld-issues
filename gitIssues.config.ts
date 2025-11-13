08e137d953e5d6ade7bb9b66a997220f58d42e59
Gareth L Devlin <33987109+GLD5000@users.noreply.github.com>
2025-11-13 12:03:45 +0000

    Update gitIssues.config.ts

--- gitIssues.config.ts
+++ gitIssues.config.ts
@@ -1,9 +1,15 @@
 export const gldIssuesConfig = {
   githubIssueCategories: [
     //GitHub Issue labels that populate category filters and progress bars
-    "targets",
-    "CPD",
-    "Issues",
-    "good first issue"
+    "cpd: targets",
+    "cpd: leadership",
+    "cpd: onboarding",
+    "cpd: soft skills",
+    "cpd: technical",
+    "bau: issues",
+    "bau: configuration",
+    "bau: qa",
+    "bau: documentation",
+    "bau: releases",
   ],
 };
