959908b96ba3cef4ff0a699ca51470a42b97ad5e
GLD5000 <garethlouisdevlin@gmail.com>
2025-11-17 22:23:15 +0000

    Add body loading #2

---
+++ src/components/github/main-section/issue-cards/card/DoubleClickTextArea.tsx
@@ -0,0 +1,103 @@
+import { useRef, useState } from "react";
+import LoadingSpinner from "../../../LoadingSpinner";
+
+export default function DoubleClickTextArea({
+  onBlurHandler,
+  onChangeHandler,
+  onClickHandler,
+  inputValue,
+  displayValue,
+  placeHolder,
+  width = "w-[30em]",
+  textAlign = "text-left",
+  isLoading = false,
+}: {
+  onBlurHandler: (e: React.FocusEvent<HTMLTextAreaElement>) => void;
+
+  onChangeHandler: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onClickHandler?: () => void;
+  inputValue: string;
+  displayValue?: string;
+  placeHolder?: string;
+  width?: string;
+  textAlign?: string;
+  isLoading?: boolean;
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      if (inputRef && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, 0);
+  };
+  if (isLoading)
+    return (
+      <div
+        className={`block font-normal text-black dark:text-white bg-transparent ${width} text-ellipsis ${textAlign} border-none`}
+      >
+        <LoadingSpinner />
+      </div>
+    );
+  return (
+    <>
+      {isEditing ? (
+        <textarea
+          placeholder={placeHolder}
+          ref={inputRef}
+          className={`block font-normal text-black dark:text-white bg-transparent ${width} text-ellipsis ${textAlign} border-none h-40`}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && e.ctrlKey) {
+              e.currentTarget.blur();
+            }
+          }}
+          onBlur={(e) => {
+            onBlurHandler(e);
+            // setTimeout(() => {
+            //     if (inputRef && inputRef.current) {
+            //         inputRef.current.blur();
+            //     }
+            // }, 0)
+            setIsEditing(false);
+          }}
+          onChange={(e) => {
+            onChangeHandler(e);
+          }}
+          onKeyDownCapture={(e) => {
+            if (e.key === " ") {
+              e.preventDefault();
+              const currentCharacter = e.currentTarget.selectionStart || 0;
+              const currentString = e.currentTarget.value;
+              const prependString = currentString.slice(0, currentCharacter);
+              const appendString = currentString.slice(currentCharacter);
+              // Insert space character manually
+              e.currentTarget.value = `${prependString} ${appendString}`;
+              // Move cursor to the end of the e.currentTarget
+              e.currentTarget.setSelectionRange(
+                currentCharacter + 1,
+                currentCharacter + 1,
+              );
+            }
+          }}
+          value={inputValue}
+        ></textarea>
+      ) : (
+        <pre
+          className={`block font-normal text-black dark:text-white bg-transparent ${width} text-ellipsis ${textAlign} border-none whitespace-pre overflow-clip cursor-text`}
+          onDoubleClick={() => {
+            handleDoubleClick();
+            if (onClickHandler) onClickHandler();
+          }}
+          onClick={(e) => {
+            // e.stopPropagation();
+            e.preventDefault();
+          }}
+        >
+          {displayValue || placeHolder || inputValue}
+        </pre>
+      )}
+    </>
+  );
+}
