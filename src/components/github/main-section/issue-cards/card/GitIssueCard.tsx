93ff2df18004791414ff7f82a935c7e02fec72a5
GLD5000 <garethlouisdevlin@gmail.com>
2025-11-17 20:42:51 +0000

    Update issue card focus behaviour

--- src/components/github/main-section/issue-cards/card/GitIssueCard.tsx
+++ src/components/github/main-section/issue-cards/card/GitIssueCard.tsx
@@ -144,9 +144,9 @@ export default function GitIssueCard({
       >
         <summary
           aria-label={`${summaryId}`}
-          className="cursor-pointer p-1 absolute top-0 right-0 bottom-0 flex flex-wrap gap-1 items-center w-7 box-border bg-white dark:bg-black group focus-within:outline-0"
+          className="cursor-pointer p-1 absolute top-0 right-0 bottom-0 flex flex-wrap gap-1 items-center w-7 box-border bg-white dark:bg-black group focus-visible:outline-0"
         >
-          <span className="text-center ease-out duration-200 group-open:rotate-180 group-open:ease-in rounded-[50%] w-full aspect-square box-border block text-neutral-500 dark:text-neutral-400 group-hover:text-black group-focus-within:text-black group-hover:dark:text-white group-focus-within:dark:text-white group-hover:border-current group-focus-within:border-current border-transparent border border-solid pointer-events-none transition">
+          <span className="text-center ease-out duration-200 group-open:rotate-180 group-open:ease-in rounded-[50%] w-full aspect-square box-border block text-neutral-500 dark:text-neutral-400 group-hover:text-black group-focus-visible:text-black group-hover:dark:text-white group-focus-visible:dark:text-white group-focus-visible:border-current border-transparent border border-solid pointer-events-none transition">
             <svg
               width="100%"
               height="100%"
