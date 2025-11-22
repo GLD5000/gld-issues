d0bfc9cc4e6aee146a0410ff6ba913fb17ba666f
GLD5000 <garethlouisdevlin@gmail.com>
2025-11-22 22:31:34 +0000

    Update task card layout

--- src/components/github/main-section/issue-cards/card/GitIssueBodyTodoList.tsx
+++ src/components/github/main-section/issue-cards/card/GitIssueBodyTodoList.tsx
@@ -9,7 +9,7 @@ export default function GitIssueBodyTodoList({
 }) {
   if (!taskList || taskList.length === 0) return null;
   return (
-    <div className="grid border border-neutral-500 dark:border-neutral-400 mx-auto border-solid p-3 rounded-lg w-[calc(100%-2rem)] overflow-x-auto">
+    <div className="grid mx-auto border-solid rounded-lg w-full overflow-x-auto">
       <h3 className="text-base text-inherit underline underline-offset-4">
         Task List
       </h3>
--- src/components/github/main-section/issue-cards/card/GitIssueCard.tsx
+++ src/components/github/main-section/issue-cards/card/GitIssueCard.tsx
@@ -91,7 +91,7 @@ export default function GitIssueCard({
   const summaryId = `${shortTitle}`; // Make unique with ${issue.number} -
   return (
     <div className="relative w-full">
-      <div className="mr-6 md:mr-0 flex flex-wrap lg:grid lg:grid-cols-[auto_1fr_auto_auto] gap-1 items-center w-[calc(100%-26px)] lg:w-full box-border bg-neutral-100 dark:bg-neutral-800">
+      <div className="mr-6 md:mr-0 flex flex-wrap lg:grid lg:grid-cols-[auto_1fr_auto_24px] items-center w-[calc(100%-26px)] lg:w-full box-border bg-neutral-100 dark:bg-neutral-800">
         <div className="w-fit h-full p-1">
           <button
             className="m-0.5 hidden md:block  text-center text-sm border border-current border-solid w-4.5 h-4.5  rounded-md bg-transparent text-neutral-500 dark:text-neutral-400 hover:text-black focus:text-black hover:dark:text-white focus:dark:text-white transition box-border"
@@ -137,7 +137,6 @@ export default function GitIssueCard({
             lastUpdated={lastUpdated}
           />
         </div>
-        <div className="w-6"></div>
       </div>
       <details
         className={classMerge(
@@ -148,7 +147,7 @@ export default function GitIssueCard({
       >
         <summary
           aria-label={`${summaryId}`}
-          className="cursor-pointer p-1 absolute top-0 right-0 bottom-0 flex flex-wrap gap-1 items-center w-7 box-border bg-white dark:bg-black hover:bg-neutral-100 hover:dark:bg-neutral-800 group focus-visible:outline-0"
+          className="cursor-pointer absolute top-0 right-0 bottom-0 flex flex-wrap gap-1 items-center w-6 box-border bg-white dark:bg-black hover:bg-neutral-100 hover:dark:bg-neutral-800 group focus-visible:outline-0"
         >
           <span className="text-center ease-out duration-200 group-open:rotate-180 group-open:ease-in rounded-[50%] w-full aspect-square box-border block text-neutral-500 dark:text-neutral-400 group-hover:text-black group-focus-visible:text-black group-hover:dark:text-white group-focus-visible:dark:text-white group-focus-visible:border-current border-transparent border border-solid pointer-events-none transition">
             <svg
@@ -172,6 +171,7 @@ export default function GitIssueCard({
             </svg>
           </span>
         </summary>
+        <div className="grid grid-cols-[1fr_24px]">
         <div className="grid gap-4 text-sm w-full box-border p-4 newDesktop:p-8">
           <div className="flex flex-wrap items-center">
             <DoubleClickInput
@@ -277,6 +277,7 @@ export default function GitIssueCard({
           >
             Github.com Issue #{issue.number} &rarr;
           </a>
+        </div>
         </div>
       </details>
     </div>
