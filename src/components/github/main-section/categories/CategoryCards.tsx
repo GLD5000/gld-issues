bf36bc2c4d3769c37f842bf99faa243571847e45
GLD5000 <garethlouisdevlin@gmail.com>
2025-12-01 22:42:29 +0000

    Remove unneeded import

--- src/components/github/main-section/categories/CategoryCards.tsx
+++ src/components/github/main-section/categories/CategoryCards.tsx
@@ -1,4 +1,4 @@
-import { Fragment, ReactElement } from "react";
+import { ReactElement } from "react";
 import SubCategoryFilters from "../../top-section/SubCategoryFilters";
 import {
   SelectiveIssuesJsonShape,
