6b2e92b1980806e53bb899cb2c51c50dff9e0184
GLD5000 <garethlouisdevlin@gmail.com>
2026-01-06 16:34:51 +0000

    Remove since date from getGithubIssues fixes #24

--- src/app/api/getGithubIssues/[slug]/route.ts
+++ src/app/api/getGithubIssues/[slug]/route.ts
@@ -1,6 +1,5 @@
 import { Octokit } from "octokit";
 import { NextResponse } from "next/server";
-// import { getIsoStringFromRelativeWeekNumber } from "@/utils/dates";
 
 export async function GET(
   request: Request,
@@ -22,8 +21,6 @@ export async function POST(): Promise<NextResponse> {
 async function getAll() {
   const username = process.env.GH_USER;
   const repo = process.env.GH_REPO;
-  // const isoStringForClosedIssues = getIsoStringFromRelativeWeekNumber();
-  // console.log("isoStringForClosedIssues:", isoStringForClosedIssues);
   if (!username) {
     return NextResponse.json(
       { error: "Username is required" },
